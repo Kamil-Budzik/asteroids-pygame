a0745a2fa3841cd128d6aa6d7695e8e92f1c2a02
Kamil-Budzik <mixboksu@gmail.com>
2025-09-30 13:53:35 +0200

    add CircleShape

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    window = pygame.display.set_mode((650, 650))
 
-    while True:
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
 
-            pygame.Surface.fill(window, "black")
-            pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
-    exit()
