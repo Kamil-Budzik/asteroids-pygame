fbe7d04769e4d82d40b16d75277dee398b45e84c
Kamil-Budzik <mixboksu@gmail.com>
2025-10-01 11:35:48 +0200

    Render asteroids

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+    LINE_WIDTH = 2
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.velocity = pygame.Vector2(0, 0)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius, Asteroid.LINE_WIDTH)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import Player
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,7 +10,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = []
+    drawable = []
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.append(player)
+    drawable.append(player)
+
+    asteroids = []
+    asteroid_field = AsteroidField(asteroids)
+    updatable.append(asteroid_field)
 
     running = True
     while running:
@@ -17,18 +27,20 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        for obj in updatable:
+            obj.update(dt)
+        for asteroid in asteroids:
+            asteroid.update(dt)
 
         screen.fill("black")
-
-        player.update(dt)
-        player.draw(screen)
-
-
+        for obj in [player] + asteroids:
+            obj.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
-
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
